ee6ad0808713952e7afcfdec9fc15fa4beb2a241
张超 <u201414176@hust.edu.cn>
2016-09-28 12:37:18 +0800

    更改服务器配置文件

--- lab/lab/settings.py
+++ lab/lab/settings.py
@@ -100,6 +100,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+# STATIC_URL = '/static/'
+# STATIC_ROOT= os.path.join(BASE_DIR,'statics/')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
